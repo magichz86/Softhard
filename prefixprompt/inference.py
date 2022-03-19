7f30b07a67e651cb6064d1942481239896a737e9
magichz86 <1046848468@qq.com>
2022-03-19 14:46:25 +0800

    + bert-score and rouge-l

---
+++ prefixprompt/inference.py
@@ -0,0 +1,41 @@
+import json
+
+from nltk.tokenize import word_tokenize
+
+from bert_score import score
+from rouge import Rouge
+
+
+def cal_bert_score(hypos, refs):
+    _, _, b_score = score(hypos, refs, lang='en', verbose=True)
+    return b_score.mean()
+
+
+def cal_rougel(hypos, refs):
+    # generated_sens = []
+    # ref_sens = []
+    # for ref, hypo in zip(refs, hypos):
+    #     tokenized_rs = []
+    #     ref = ref.split("\n")
+    #     for r in ref:
+    #         tokenized_rs.append(word_tokenize(r))
+    #     hypo = word_tokenize(hypo)
+    #     generated_sens.append(hypo)
+    #     ref_sens.append(tokenized_rs)
+    rouge = Rouge()
+    scores = rouge.get_scores(hypos, refs, avg=True)
+    return scores["rouge-l"]["f"]
+
+# for ref, hypo in zip(refs, hypos):
+#     tokenized_rs = []
+#     ref = ref.split("\n")
+#     for r in ref:
+#         tokenized_rs.append(word_tokenize(r))
+#     hypo = word_tokenize(hypo)
+#     try:
+#         sc = sentence_bleu(tokenized_rs, hypo, smoothing_function=smoothie)
+#     except ValueError:
+#         logger.warning("math domain error in bleu, set to 0.0. generated sentence: {}".format(hypo))
+#         sc = 0.0
+#     scores.append(sc)
+# score = sum(scores) / len(scores)
--- prefixprompt/run.py
+++ prefixprompt/run.py
@@ -14,9 +14,16 @@ from openprompt.utils.metrics import generation_metric
 from transformers import AdamW
 from transformers.optimization import get_linear_schedule_with_warmup
 from openprompt.prompts import MixedTemplate
+
+from inference import cal_rougel, cal_bert_score
+
+# TODO 2: + test model on PathQuestion
+# TODO 3: + test model on 2-hop and 3-hop separately
+
 parser = argparse.ArgumentParser("")
 parser.add_argument("--use_cuda", type=bool, default=True)
 parser.add_argument("--lr", type=float, default=5e-5)
+parser.add_argument("--num_token", type=int, default=10)
 parser.add_argument("--plm_eval_mode", action="store_true")
 parser.add_argument("--model", type=str, default='t5')  # tested model are gpt2/t5
 parser.add_argument("--model_name_or_path", default='t5-base')
@@ -40,7 +47,7 @@ def main(generation_arguments):
 
     # Instantiating the PrefixTuning Template !
     template_text = ' [CLS] {"placeholder":"text_a"} [SEP] {"placeholder":"text_b"} [SEP] {"special": "<eos>"} {"mask"}'
-    mytemplate = PrefixTuningTemplate(model=plm,  tokenizer=tokenizer, text=template_text, num_token=10, using_decoder_past_key_values=False)
+    mytemplate = PrefixTuningTemplate(model=plm,  tokenizer=tokenizer, text=template_text, num_token=args.num_token, using_decoder_past_key_values=False)
 
     # To better understand how does the template wrap the example, visualize one instance.
     wrapped_example = mytemplate.wrap_one_example(dataset['train'][0])
@@ -70,48 +77,48 @@ def main(generation_arguments):
         prompt_model = prompt_model.cuda()
 
     # only include the template's parameters in training.
-    no_decay = ["bias", "LayerNorm.weight"]
-    optimizer_grouped_parameters = [
-    {
-        "params": [p for n, p in mytemplate.named_parameters() if (not any(nd in n for nd in no_decay)) and p.requires_grad],
-        "weight_decay": 0.0,
-    },
-    {
-        "params": [p for n, p in mytemplate.named_parameters() if any(nd in n for nd in no_decay) and p.requires_grad],
-        "weight_decay": 0.0,
-    },
-    ]
+    # no_decay = ["bias", "LayerNorm.weight"]
+    # optimizer_grouped_parameters = [
+    # {
+    #     "params": [p for n, p in mytemplate.named_parameters() if (not any(nd in n for nd in no_decay)) and p.requires_grad],
+    #     "weight_decay": 0.0,
+    # },
+    # {
+    #     "params": [p for n, p in mytemplate.named_parameters() if any(nd in n for nd in no_decay) and p.requires_grad],
+    #     "weight_decay": 0.0,
+    # },
+    # ]
+    #
+    # optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr, eps=1e-8)
+    # tot_step = len(train_dataloader)*5
+    # scheduler = get_linear_schedule_with_warmup(optimizer, 0, tot_step)
+    #
+    # # training and generation.
+    # global_step = 0
+    # tot_loss = 0
+    # log_loss = 0
+    # for epoch in range(5):
+    #     prompt_model.train()
+    #     for step, inputs in enumerate(train_dataloader):
+    #         global_step += 1
+    #         if args.use_cuda:
+    #             inputs = inputs.cuda()
+    #         loss = prompt_model(inputs)
+    #         # loss.requires_grad_(True)
+    #         loss.backward()
+    #         tot_loss += loss.item()
+    #         torch.nn.utils.clip_grad_norm_(mytemplate.parameters(), 1.0)
+    #         optimizer.step()
+    #         scheduler.step()
+    #         optimizer.zero_grad()
+    #         if global_step % 500 == 0:
+    #             print("Epoch {}, global_step {} average loss: {} lr: {}".format(epoch, global_step, (tot_loss-log_loss)/500, scheduler.get_last_lr()[0]), flush=True)
+    #             log_loss = tot_loss
+    #     if not os.path.isdir("saved_dict"):
+    #         os.makedirs("saved_dict")
+    #     torch.save(prompt_model.state_dict(), './saved_dict/' + "Epoch" + str(epoch) + "_" + args.model_name_or_path + '.pt')
 
-    optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr, eps=1e-8)
-    tot_step = len(train_dataloader)*5
-    scheduler = get_linear_schedule_with_warmup(optimizer, 0, tot_step)
-
-    # training and generation.
-    global_step = 0
-    tot_loss = 0
-    log_loss = 0
-    for epoch in range(5):
-        prompt_model.train()
-        for step, inputs in enumerate(train_dataloader):
-            global_step += 1
-            if args.use_cuda:
-                inputs = inputs.cuda()
-            loss = prompt_model(inputs)
-            # loss.requires_grad_(True)
-            loss.backward()
-            tot_loss += loss.item()
-            torch.nn.utils.clip_grad_norm_(mytemplate.parameters(), 1.0)
-            optimizer.step()
-            scheduler.step()
-            optimizer.zero_grad()
-            if global_step % 500 == 0:
-                print("Epoch {}, global_step {} average loss: {} lr: {}".format(epoch, global_step, (tot_loss-log_loss)/500, scheduler.get_last_lr()[0]), flush=True)
-                log_loss = tot_loss
-        if not os.path.isdir("saved_dict"):
-            os.makedirs("saved_dict")
-        torch.save(prompt_model.state_dict(), './saved_dict/' + "Epoch" + str(epoch) + "_" + args.model_name_or_path + '.pt')
-
-    #prompt_model.load_state_dict(torch.load('./saved_dict/sq-Epoch4_t5-base.pt'))
+    prompt_model.load_state_dict(torch.load('./saved_dict/sq-Epoch4_t5-base.pt'))
     generated_sentence, groundtruth_sentence = evaluate(prompt_model, test_dataloader, generation_arguments)
 
     with open(f"Generated_sentence_simplequestion.json", 'w') as f:
@@ -134,8 +141,16 @@ def evaluate(prompt_model, dataloader, generation_arguments):
         generated_sentence.extend(output_sentence)
         groundtruth_sentence.extend(inputs['tgt_text'])
         #print("evaluate step " + str(step))
-    score = generation_metric(generated_sentence, groundtruth_sentence, "sentence_bleu")
-    print("test_bleu_4: ", 100*score, flush=True)
+    print(generated_sentence[0])
+    print(groundtruth_sentence[0])
+    bleu4 = generation_metric(generated_sentence, groundtruth_sentence, "sentence_bleu")
+    bert_score_F1 = cal_bert_score(generated_sentence, groundtruth_sentence)
+    rl_score_F1 = cal_rougel(generated_sentence, groundtruth_sentence)
+
+    print("test_bleu_4: ", 100*bleu4, flush=True)
+    print("test_bert_score: "+str(bert_score_F1))
+    print("test_rouge-l: " + str(rl_score_F1))
+
     return generated_sentence, groundtruth_sentence
 
 
